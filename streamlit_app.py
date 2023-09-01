195a57699070a48c5fddda847ef75d848da3f572
spamula09 <96599998+spamula09@users.noreply.github.com>
2023-09-01 10:37:42 -0400

    Updated images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner!!")
-streamlit.title("Breakfast items")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spianch and Rocket smoothie")
-streamlit.text("Hardboiled Free range egg")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
