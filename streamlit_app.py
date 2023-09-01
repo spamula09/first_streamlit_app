00bb907629284dfddb38235e1f196b2ae1bb2ab0
spamula09 <96599998+spamula09@users.noreply.github.com>
2023-09-01 10:35:02 -0400

    Added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("My parent's New Healthy Diner!!")
+streamlit.title("My Parents New Healthy Diner!!")
+streamlit.title("Breakfast items")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spianch and Rocket smoothie")
+streamlit.text(Hardboiled Free range egg")
