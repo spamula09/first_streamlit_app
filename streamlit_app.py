42a55cd76fb6399047791cb668b73445a0c86830
spamula09 <96599998+spamula09@users.noreply.github.com>
2023-09-03 11:52:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,9 @@ except URLError as e:
 
 def isnert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute(f"insert into fruit_load_list values ({new_fruit})")
+    statement =f"insert into fruit_load_list values (''{new_fruit}'')"
+    streamlit.text(statement)
+    my_cur.execute(statement)
     return ("Thanks for adding "+ new_fruit)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button("Add fruit to the list"):
